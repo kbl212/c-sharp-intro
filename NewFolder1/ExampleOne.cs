3b82f8bd17c3b3d9d61954b5e49c2ce44fea2369
Kyle <kyle.lauritzen@gmail.com>
2016-02-07 23:10:24 -0700

    first C# project, with Visual Studio, ...Hello World...

---
+++ NewFolder1/ExampleOne.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication1.NewFolder1
+{
+    class ExampleOne
+    {
+        static void Main()
+        {
+            Console.Write("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
